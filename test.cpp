394379ca006104e0595e4e81185abf422e5efb37
carlagranadossoler <138595058+carlagranadossoler@users.noreply.github.com>
2023-07-05 15:50:22 +0200

    Add files via upload

--- test.cpp
+++ test.cpp
@@ -3,16 +3,7 @@
 using namespace std;
 
 int main(int argc, char* argv[]){
-    string input = "";
-    string output = "";
-    if (argc > 2){
-        input = argv[1];
-        output = argv[2];
-    }
-    else if (argc > 1){ 
-        output = argv[1];
-    }
     string ruta = "main.cpp";
-    string command = "g++ " + ruta + " -o main.exe && main.exe " + input + " " + output;
+    string command = "g++ " + ruta + " -o main.exe && main.exe";
     int result = system(command.c_str());
 }
